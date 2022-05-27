d4eb604c3497a2375cf3b5d315c017723462b1fc
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-05-27 19:23:47 -0400

    updated

--- Practice/Practice.java
+++ Practice/Practice.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Practice{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner myObj = new Scanner((System.in));
+
+        System.out.println("Enter your Name: ");
+        String name = myObj.nextLine();
+
+        System.out.println("Enter value of a: ");
+        int a = myObj.nextInt();
+
+        System.out.println("Enter value of b: ");
+        int b = myObj.nextInt();
+
+        System.out.println("Name: "+name);
+        System.out.println("a: "+a);
+        System.out.println("b: "+b);
+
+        int c = a + b;
+        System.out.println("c: "+c);
     }
 }
